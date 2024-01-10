05c835b8024cb8d9a4c97e3af558eeeaf3a17ff3
Edleron <Edleron@hotmail.com>
2024-01-11 00:36:19 +0300

    understand : route, routind, add controller systems

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace McCrudFire.Controllers
+{
+    [Route("Products")]
+    public class ProductsController : ControllerBase 
+    {
+        [Route("MerhabaOne")]
+        public string MerhabaOne()
+        {
+            return "Merhaba - 1";
+        }
+
+        [Route("MerhabaTwo")]
+        public string MerhabaTwo()
+        {
+            return "Merhaba - 2";
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -16,26 +16,23 @@ namespace McCrudFire
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();   
+            }
+
             app.UseRouting(); // Bize gelen request'in hangi route ile eþleþdiðinin bilgsini verir.
 
             // Aþaðýdaki kod route'nýn çalýþtýrýlmasýný saðlar.
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-
-                // Ýkinci bir end point ekledik
-                endpoints.MapGet("/products", async context =>
-                {
-                    await context.Response.WriteAsync("Products List!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
